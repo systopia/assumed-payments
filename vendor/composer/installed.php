14df7fea30278573a08f1f98d9b877df2f6e534b
Jens Schuppe <schuppe@systopia.de>
2026-02-12 15:22:52 +0100

    Set version to 1.0.0-alpha1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/assumed-payments',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7a7fd5021d466acf71121ab0b042e09ab3d04d79',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/assumed-payments' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7a7fd5021d466acf71121ab0b042e09ab3d04d79',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
