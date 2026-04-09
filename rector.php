d9686c8faa8b1b4ab91ae40377f8fe6060930eae
ArthegaAsdweri <daniel.hahn86@gmx.de>
2026-04-09 09:00:28 +0200

    Fixed CI Workflow

--- rector.php
+++ rector.php
@@ -6,9 +6,7 @@ use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
-use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
-use Rector\TypeDeclaration\Rector\StmtsAwareInterface\SafeDeclareStrictTypesRector;
 use Rector\Set\ValueObject\SetList;
 
 return RectorConfig::configure()
@@ -20,16 +18,12 @@ return RectorConfig::configure()
     __DIR__ . '/tests',
   ])
   ->withSkip([
-    ClassPropertyAssignToConstructorPromotionRector::class => NULL,
     AddVoidReturnTypeWhereNoReturnRector::class => NULL,
     RemoveUselessParamTagRector::class => NULL,
     RemoveUselessReturnTagRector::class => NULL,
     AbsolutizeRequireAndIncludePathRector::class => [
       __DIR__ . '/assumed_payments.php',
     ],
-  ])
-  ->withRules([
-    SafeDeclareStrictTypesRector::class,
   ])
   ->withSets([
     SetList::CODE_QUALITY,
